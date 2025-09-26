9813a27d21a6554ab95ba5492ad9e88f7eaa3ef4
Danikarat <danieldzotsi24@gmail.com>
2025-09-26 07:58:58 +0000

    changed the database to postgresql

--- ShortLink/ShortLink/settings.py
+++ ShortLink/ShortLink/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&$^v)39#^%werr@lm49f%0n+o*x=tcoii(5=bi&mqw##cqvgqy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -75,10 +76,11 @@ WSGI_APPLICATION = 'ShortLink.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",  # fallback for local dev
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
